89e792857db0ed8a4ece23e27fd25fcf88d89e4f
gustavbarbosaa <gustavoaraujohab@gmail.com>
2023-04-17 23:08:01 -0300

    style: estilização de sidebar

--- src/components/Pages/index.vue
+++ src/components/Pages/index.vue
@@ -1,8 +1,14 @@
+<!-- eslint-disable max-len -->
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div id="pages" class="flex items-center pl-1">
-    <img :src="img" :alt="alt">
-    <h2>{{text}}</h2>
+  <div id="pages">
+    <a href="/">
+      <!-- p-3 mx-1 my-3 pl-3 -->
+      <div class="flex items-center justify-start gap-2 w-full h-12 px-2 my-3 font-bold text-sm text-font-sidebar-color rounded-md hover:bg-gray-700 hover:text-gray-300">
+        <img :src="img" alt="img">
+        <h2>{{text}}</h2>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -13,8 +19,14 @@ export default defineComponent({
   name: 'PagesSideBar',
   props: {
     img: String,
-    alt: String,
     text: String,
   },
 });
 </script>
+
+<style scoped>
+  #pages img{
+    width: 20px;
+    height: 20px;
+  }
+</style>
--- src/components/SideBar/index.vue
+++ src/components/SideBar/index.vue
@@ -5,19 +5,41 @@
       <img src="../../assets/images/icon-e-ga.png" alt="" class="w-11 h-11">
       <h2 class="text-font-sidebar-color font-bold text-sm">Projeto e-GA</h2>
     </div>
-    <div id="pages-side-bar">
-
+    <div id="pages-side-bar" class="mx-2 border-b border-border-color">
+      <PagesSideBar :img="require('../../assets/images/arquivos.png')" :text="textArq"/>
+      <PagesSideBar :img="require('../../assets/images/cadastro.png')" :text="textCad"/>
+      <PagesSideBar :img="require('../../assets/images/financeiro.png')" :text="textFin"/>
+      <PagesSideBar :img="require('../../assets/images/consultas.png')" :text="textCons"/>
+      <PagesSideBar :img="require('../../assets/images/frequencia.png')" :text="textFreq"/>
+      <PagesSideBar :img="require('../../assets/images/notas.png')" :text="textNot"/>
+      <PagesSideBar :img="require('../../assets/images/plano.png')" :text="textPlano"/>
     </div>
-    <div id="logout">
-
+    <div id="logout" class="w-full h-12 px-2 my-3">
+      <PagesSideBar :img="require('../../assets/images/forum.png')" :text="textForum" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import PagesSideBar from '../Pages/index.vue';
 
 export default defineComponent({
   name: 'SideBar',
+  components: {
+    PagesSideBar,
+  },
+  data() {
+    return {
+      textArq: 'Meus Arquivos',
+      textCad: 'Meu Cadastro',
+      textFin: 'Financeiro',
+      textCons: 'Consultas',
+      textFreq: 'Frequência',
+      textNot: 'Notas',
+      textPlano: 'Plano de Aula',
+      textForum: 'Fórum de Turmas',
+    };
+  },
 });
 </script>
